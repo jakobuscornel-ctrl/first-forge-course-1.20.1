c1dd626ec120ac1ac5a6183a3267c6dfe6bf96ae
jakobuscornel-ctrl <jakobuscornel@gmail.com>
2026-08-31 15:04:49 +0200

    Just made some changes

--- src/main/java/net/chameleon/mymccourse/block/ModBlocks.java
+++ src/main/java/net/chameleon/mymccourse/block/ModBlocks.java
@@ -10,10 +10,17 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MyMcCourseMod.MOD_ID);
 
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
             return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
